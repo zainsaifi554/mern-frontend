499552c863e9356bceaa1ad693d166abb90ed038
zainsaifi554 <zainsaifi235798@gmail.com>
2025-08-04 12:49:03 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  base: "./",
+  build: {
+    outDir: "dist",
+  },
   plugins: [
     tailwindcss(),
-  ],
+  ]
 })
